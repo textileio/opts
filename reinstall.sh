3ac265c994187592dcebd728da88780cb1ec0dd2
Sander Pick <sanderpick@gmail.com>
2019-02-06 15:48:19 -0800

    upgrade: specify a branch to build and install

--- reinstall.sh
+++ reinstall.sh
@@ -2,25 +2,41 @@
 
 set -e
 
-while getopts r: option
+while getopts r:b: option
 do
 case "${option}"
 in
 r) RELEASE=${OPTARG};;
+b) BRANCH=${OPTARG};;
 esac
 done
 
-[[ -z "$RELEASE" ]] && { echo "Please specify a release tag, e.g., -r v1.0.0" ; exit 1; }
+[[ -z "$RELEASE" && -z "$BRANCH" ]] && { echo "Please specify a release tag or a branch, e.g., -r v1.0.0 or -b master" ; exit 1; }
 
-# install
-wget https://github.com/textileio/textile-go/releases/download/"$RELEASE"/textile-go_"$RELEASE"_linux-amd64.tar.gz
-tar xvfz textile-go_"$RELEASE"_linux-amd64.tar.gz
-rm textile-go_"$RELEASE"_linux-amd64.tar.gz
-sudo ./install.sh
+if [ ! -z "$RELEASE" ]; then
 
+  # install from GH release
+  wget https://github.com/textileio/textile-go/releases/download/"$RELEASE"/textile-go_"$RELEASE"_linux-amd64.tar.gz
+  tar xvfz textile-go_"$RELEASE"_linux-amd64.tar.gz
+  rm textile-go_"$RELEASE"_linux-amd64.tar.gz
+  sudo ./install.sh
+
+else
+
+  # pull latest on branch, build local
+  source ~/.bash_profile
+  cd go/src/github.com/textileio/textile-go
+  git stash
+  git checkout "$BRANCH" && git pull origin "$BRANCH"
+  make setup && make build
+  sudo make install
+
+fi
+
+# restart service
 sudo systemctl restart textile
 
-# echo new node info
+# echo node info
 sleep 5
 echo "version:" $(textile version)
 echo "peer:   " $(textile peer)
--- upgrade.sh
+++ upgrade.sh
@@ -2,22 +2,27 @@
 
 set -e
 
-while getopts k:r:p: option
+while getopts k:r:b:p: option
 do
 case "${option}"
 in
 k) KEY=${OPTARG};;
 r) RELEASE=${OPTARG};;
+b) BRANCH=${OPTARG};;
 p) IP=${OPTARG};;
 esac
 done
 
 [[ -z "$KEY" ]] && { echo "Please specify an SSH key, e.g., -k mykey.pem" ; exit 1; }
-[[ -z "$RELEASE" ]] && { echo "Please specify a release tag, e.g., -r v1.0.0" ; exit 1; }
+[[ -z "$RELEASE" && -z "$BRANCH" ]] && { echo "Please specify a release tag or a branch, e.g., -r v1.0.0 or -b master" ; exit 1; }
 [[ -z "$IP" ]] && { echo "Please specify a public host IP address, e.g., -p 18.144.12.134" ; exit 1; }
 
 # upload installer
 scp -i "$KEY" reinstall.sh ec2-user@"$IP":~/
 
 # run installer
-echo "./reinstall.sh -r $RELEASE" | ssh -i "$KEY" ec2-user@"$IP" /bin/bash
+if [ ! -z "$RELEASE" ]; then
+  echo "./reinstall.sh -r $RELEASE" | ssh -i "$KEY" ec2-user@"$IP" /bin/bash
+else
+  echo "./reinstall.sh -b $BRANCH" | ssh -i "$KEY" ec2-user@"$IP" /bin/bash
+fi
